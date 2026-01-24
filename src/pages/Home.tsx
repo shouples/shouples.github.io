5b24b3989778b38e790b3016af94804379ff87db
Dave Shoup <dave.shoup@gmail.com>
2026-01-24 17:49:48 -0500

    use different icon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { PostList } from "../components/PostList";
+import { posts } from "../data/posts";
+
 export function Home() {
   return (
     <>
       <p>Hello. 👋 Thanks for checking out my page.</p>
-      <p>I hope to add some hobby- and project-related content here over time.</p>
+      <PostList posts={posts} direction="vertical" />
     </>
   );
 }
